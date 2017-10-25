23d33e482bb6e7f13b7394232a25da7776615e64
sunqi@snailgame.net <sunqi@snailgame.net>
2017-10-25 11:51:21 +0800

    serverScokect

--- ognltest/src/main/java/com/practice/netty/ServerSocketTest.java
+++ ognltest/src/main/java/com/practice/netty/ServerSocketTest.java
@@ -1,6 +1,7 @@
 package com.practice.netty;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.Charset;
@@ -13,12 +14,24 @@ public class ServerSocketTest {
 	public static void main(String[] args) throws IOException {
 		ServerSocket server = ServerSocketFactory.getDefault().createServerSocket(8889);
 		while (true){
-			Socket soc =  server.accept();
+			final Socket soc =  server.accept();
 			System.out.println("connected !");
-			byte [] b = new byte[1024];
-			int length = soc.getInputStream().read(b);
-			System.out.println("get message length : " + length);
-			System.out.println("get message : " + new String(b,CharsetUtil.UTF_8));
+			new Thread(()->{
+				byte [] b = new byte[1024];
+				int length;
+				try {
+					InputStream in = soc.getInputStream();
+					while((length = in.read(b))> 0){
+						System.out.println("get message length : " + length);
+						System.out.println("get message : " + new String(b,CharsetUtil.UTF_8));
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			},"Thread#" + soc.hashCode()).start();
+			
 		}
 		
 	}
