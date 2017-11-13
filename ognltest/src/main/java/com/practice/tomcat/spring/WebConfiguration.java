94b7583a73acc210e1a57c5df48a941ce71dd8b4
sunqi@snailgame.net <sunqi@snailgame.net>
2017-11-13 18:26:59 +0800

    ok

--- ognltest/src/main/java/com/practice/tomcat/spring/WebConfiguration.java
+++ ognltest/src/main/java/com/practice/tomcat/spring/WebConfiguration.java
@@ -1,8 +1,17 @@
 package com.practice.tomcat.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class WebConfiguration {
-
+	@Bean
+    public JedisConnectionFactory connectionFactory() {
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		return factory;
+    }
 }
