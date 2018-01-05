d3ac3b4eb8bc22a539766db144b0cdf37448d40a
sunqi@snailgame.net <sunqi@snailgame.net>
2018-01-05 14:08:21 +0800

    hadoop

--- ognltest/src/main/java/com/practice/hadoop/common/HadoopTest1.java
+++ ognltest/src/main/java/com/practice/hadoop/common/HadoopTest1.java
@@ -5,6 +5,7 @@ import java.net.URI;
 
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Hdfs;
 import org.apache.hadoop.fs.LocatedFileStatus;
@@ -17,12 +18,26 @@ public class HadoopTest1 {
 
 	@Test
 	public void test() throws IOException {
-		
+
+		System.setProperty("HADOOP_USER_NAME", "hdfs");
 		Configuration config = new Configuration();
 		FileSystem fs = FileSystem.get(config);
+		
 		System.out.println("FileSystem Type : " + fs.getClass());
-		Path path = new Path(URI.create("/"));
+
+		Path path = new Path(URI.create("/test"));
+		FSDataOutputStream dsDos = fs.create(path);
+		dsDos.writeBytes("qwedqweqw");
+
+		path = new Path(URI.create("/test"));
 		RemoteIterator<LocatedFileStatus> itr = fs.listFiles(path, true);
+		while (itr.hasNext()) {
+			LocatedFileStatus lfs = itr.next();
+			System.out.println(lfs.getPath());
+			System.out.println(lfs);
+		}
+		
+
 	}
 
 }
