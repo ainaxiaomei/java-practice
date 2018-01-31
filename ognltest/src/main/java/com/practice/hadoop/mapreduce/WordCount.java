6bc016d2435db5b27c546026cb5fa13d3b8290b9
vergil <ainaxiaomei@gmail.com>
2018-01-31 22:18:33 +0800

    mapreduce

--- ognltest/src/main/java/com/practice/hadoop/mapreduce/WordCount.java
+++ ognltest/src/main/java/com/practice/hadoop/mapreduce/WordCount.java
@@ -1,13 +1,14 @@
 package com.practice.hadoop.mapreduce;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,53 +16,68 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
-	
-	static class WCMapper extends Mapper<Object,Text,Text,IntWritable>{
+
+	static class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
 		@Override
 		protected void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
-			StringTokenizer split = new StringTokenizer(value.toString(), " ");
-			IntWritable one = new IntWritable(1);
-			if(split.hasMoreTokens()) {
-				String word = split.nextToken();
-				Text t = new Text(word);
-				context.write(t, one);
+
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
 			}
+
 		}
 
-		
 	}
-	
-	static class WCReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+	static class WordReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
 
 		@Override
-		protected void reduce(Text key, Iterable<IntWritable> list,
-				Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
-			
+		protected void reduce(Text key, Iterable<IntWritable> values,
+				Reducer<Text, IntWritable, Text, IntWritable>.Context context)
+				throws IOException, InterruptedException {
 			int sum = 0;
-			while(list.iterator().hasNext()) {
-				sum = list.iterator().next().get() + sum;
+			for (IntWritable val : values) {
+				sum += val.get();
 			}
-			
-			IntWritable result = new IntWritable();
 			result.set(sum);
 			context.write(key, result);
-		
 		}
-		
+
 	}
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		while (args.length < 2) {
+			System.out.println("参数错误");
+			System.exit(0);
+		}
+
 		Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "word count");
-	    job.setJarByClass(WordCount.class);
-	    job.setMapperClass(WCMapper.class);
-	    job.setCombinerClass(WCReducer.class);
-	    job.setReducerClass(WCReducer.class);
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		conf.set(Job.NUM_REDUCES, "1");
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(WordMapper.class);
+		job.setReducerClass(Reducer.class);
+		job.setJarByClass(WordCount.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setCombinerClass(Reducer.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+
+		Path outPath = new Path(args[1]);
+		System.setProperty("HADOOP_USER_NAME", "hdfs");
+		FileSystem fs = outPath.getFileSystem(conf);
+		if (fs.exists(outPath)) {
+			fs.delete(outPath, true);
+		}
+
+		FileOutputFormat.setOutputPath(job, outPath);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
