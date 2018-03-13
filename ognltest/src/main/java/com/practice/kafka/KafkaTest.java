416f0d5da913a2b5e250ba57bc930d2501718505
vergil <ainaxiaomei@gmail.com>
2018-03-13 21:53:34 +0800

    schedule

--- ognltest/src/main/java/com/practice/kafka/KafkaTest.java
+++ ognltest/src/main/java/com/practice/kafka/KafkaTest.java
@@ -1,8 +1,12 @@
 package com.practice.kafka;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -10,6 +14,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.PartitionInfo;
 import org.junit.Test;
 
 public class KafkaTest {
@@ -94,16 +99,34 @@ public class KafkaTest {
 
 	}
 
+	
+	/**
+	 * 使用自动提交的consumer,并使用subscribe自动进行组管理
+	 * 默认会从partition的最后开始消费
+	 */
 	@Test
 	public void kafkaConsumerTest() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "192.168.5.23:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.5.23:9092");
 		props.put("group.id", "test");
 		props.put("enable.auto.commit", "true");
 		props.put("auto.commit.interval.ms", "1000");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		
+		
+		/**
+		 * 获取topic信息
+		 */
+		
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		
+		Map<String,List<PartitionInfo>> map = consumer.listTopics();
+		map.forEach((topic,list) ->{
+			System.out.println("topic : " + topic + "; " + "partition : "+ list);
+		});
+		
+		
 		consumer.subscribe(Arrays.asList("sunqi"));
 		while (true) {
 			ConsumerRecords<String, String> records = consumer.poll(100);
