b62ba82ce57cd296fd6f71afc418c5549d795bdd
sunqi@asia-isp.com <sunqi@asia-isp.com>
2018-03-17 17:16:41 +0800

    storm

--- ognltest/src/main/java/com/practice/storm/Test.java
+++ ognltest/src/main/java/com/practice/storm/Test.java
@@ -8,11 +8,14 @@ import java.util.concurrent.TimeUnit;
 import backtype.storm.LocalCluster;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBatchBolt;
 import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 
@@ -26,16 +29,65 @@ public class Test {
 		TopologyBuilder builder = new TopologyBuilder();
 		
 		builder.setSpout("wordSpout", new WordSpout());
+		builder.setBolt("wordupperbolt", new WorderUpperBolt()).localOrShuffleGrouping("wordSpout");
 		StormTopology topology = builder.createTopology();
 		
 		LocalCluster cluster = new LocalCluster();
 		Map map = new HashMap<>();
 		cluster.submitTopology("word-count", map, topology);
+		
 	}
 	
 	
+	static class WorderUpperBolt implements IRichBatchBolt{
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private OutputCollector collector;
+
+		@Override
+		public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+			this.collector = collector;
+		}
+
+		@Override
+		public void execute(Tuple input) {
+			
+			
+			String word = input.getStringByField("word");
+			System.out.println("WorderUpperSpout get input :" + word);
+			Values values = new Values(word.toUpperCase());
+			System.out.println("1231231");
+			collector.emit(values);
+			
+			
+		}
+
+		@Override
+		public void cleanup() {
+			
+		}
+
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("word"));
+		}
+
+		@Override
+		public Map<String, Object> getComponentConfiguration() {
+			return null;
+		}
+		
+	}
+	
 	static class WordSpout implements IRichSpout{
 		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private SpoutOutputCollector collector ;
 
 		@Override
@@ -67,7 +119,7 @@ public class Test {
 		    
 		    Random radom = new Random();
 		    String value = str[radom.nextInt(str.length)];
-		    System.err.println("word spout emit : " + value);
+		    System.out.println("word spout emit : " + value);
 		    values.add(value);
 			collector.emit(values);
 			
@@ -92,7 +144,7 @@ public class Test {
 
 		@Override
 		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("world"));
+			declarer.declare(new Fields("word"));
 		}
 
 		@Override
