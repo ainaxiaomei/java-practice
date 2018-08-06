2a41812f42866924f43c77e1d420cbf5cc912837
sunqi@asia-isp.com <sunqi@asia-isp.com>
2018-08-06 11:27:40 +0800

    调整jar包冲突

--- ognltest/src/main/java/com/practice/jersey/JeseryClientTest1.java
+++ ognltest/src/main/java/com/practice/jersey/JeseryClientTest1.java
@@ -4,13 +4,18 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
 import org.junit.Test;
+
 
 public class JeseryClientTest1 {
 
 	@Test
 	public void test() {
-		Client client = ClientBuilder.newClient();
+		ClientConfig config = new ClientConfig();
+		config.property( ClientProperties.PROXY_URI, "http://0.0.0.0:8888" );
+		Client client = ClientBuilder.newClient( config );
 		WebTarget webTarget = client.target("http://www.baidu.com");
 		String res = webTarget.request().get(String.class);
 		System.out.println(res);
