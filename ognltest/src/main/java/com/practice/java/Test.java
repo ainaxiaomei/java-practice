f54c5253d93a4f5c5604e9e9e6e87e7335a858d8
sunqi@asia-isp.com <sunqi@asia-isp.com>
2018-10-09 15:20:40 +0800

    netty

---
+++ ognltest/src/main/java/com/practice/java/Test.java
@@ -0,0 +1,11 @@
+package com.practice.java;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String str = new String("token\u5931\u6548\uff0c\u8bf7\u91cd\u65b0\u83b7\u53d6!");
+		System.out.println(str);
+		System.out.println(System.currentTimeMillis());
+		
+	}
+}
