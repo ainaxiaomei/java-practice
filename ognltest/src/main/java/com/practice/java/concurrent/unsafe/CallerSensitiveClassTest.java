b79e070db1ce5e538f8b9fe0a91b1454b1740b2b
sunqi@asia-isp.com <sunqi@asia-isp.com>
2019-02-18 20:19:14 +0800

    ok

--- ognltest/src/main/java/com/practice/java/concurrent/unsafe/CallerSensitiveClassTest.java
+++ ognltest/src/main/java/com/practice/java/concurrent/unsafe/CallerSensitiveClassTest.java
@@ -1,22 +1,16 @@
 package com.practice.java.concurrent.unsafe;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-
-import sun.misc.Unsafe;
 
 public class CallerSensitiveClassTest {
 	
 	
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, NoSuchFieldException {
-//		ClassLoader extClassLoader = Thread.currentThread().getContextClassLoader().getParent();
-//		Class c = extClassLoader.loadClass("com.practice.java.concurrent.unsafe.CallerSensitive");
-//		CallerSensitiveClass test  = (CallerSensitiveClass) c.getConstructor().newInstance();
-//		test.callerSensitiveMethod();
-		
-		 Field f = Unsafe.class.getDeclaredField("theUnsafe");
-		 f.setAccessible(true);
-		 Unsafe unsafe = (Unsafe) f.get(null);
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		System.setProperty("java.ext.dirs", ".E:\\workspace\\java-practice\\ognltest\\target\\classes\\com\\practice\\java\\concurrent\\unsafe;$JAVA_HOME/jre/lib/ext");
+		ClassLoader extClassLoader = Thread.currentThread().getContextClassLoader().getParent();
+		Class c = extClassLoader.loadClass("com.practice.java.concurrent.unsafe.CallerSensitiveClass");
+		CallerSensitiveClass test  = (CallerSensitiveClass) c.getConstructor().newInstance();
+		test.callerSensitiveMethod();
 		
 	}
 	
