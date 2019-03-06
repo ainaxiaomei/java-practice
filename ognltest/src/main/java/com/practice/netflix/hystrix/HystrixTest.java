0341961eeed8f064528b498790224bf1ce1d1e26
sunqi@asia-isp.com <sunqi@asia-isp.com>
2019-03-06 14:28:57 +0800

    hystrix

--- ognltest/src/main/java/com/practice/netflix/hystrix/HystrixTest.java
+++ ognltest/src/main/java/com/practice/netflix/hystrix/HystrixTest.java
@@ -1,14 +1,14 @@
 package com.practice.netflix.hystrix;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommand.Setter;
 import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixObservableCommand;
-
-import rx.Observable;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 
 public class HystrixTest {
 	
@@ -17,15 +17,60 @@ public class HystrixTest {
 		
 		
 		System.out.println(" --- 主线程 " + Thread.currentThread());
-		HelloCommand command = new HelloCommand("commonCommand");
-		String str = command.execute();
-		System.out.println(str);
 		
 		
-		HelloObserverCommand observerCommand = new HelloObserverCommand("observerCommand");
-		observerCommand.observe().subscribe((value)->{
-			System.out.println(value);
-		});
+		//获取域名列表命令
+		Setter setter = Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("L3"))
+		.andCommandKey(HystrixCommandKey.Factory.asKey("domainList"))
+		.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("L3-list-threadPool"))
+		.andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(10));
+		
+		HystrixRequestContext context = HystrixRequestContext.initializeContext();
+		
+		HystrixCommand<String> domainCommand = new HystrixCommand<String>(setter) {
+
+			@Override
+			protected String run() throws Exception {
+				
+				System.out.println(" --- domain command thread " + Thread.currentThread());
+				return "baidu.com";
+			}
+
+			@Override
+			protected String getFallback() {
+				return "获取域名失败！";
+			}
+
+			@Override
+			protected String getCacheKey() {
+				return "sunqi.com";
+			}
+			
+			
+			
+			
+			
+		};
+		
+		String str = domainCommand.execute();
+		
+		System.out.println(domainCommand.getClass().getSimpleName() + " return " + str);
+		
+		
+		setter.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("L3-refresh-threadPool"));
+		HystrixCommand<String> refreshCommand = new HystrixCommand<String>(setter){
+
+			@Override
+			protected String run() throws Exception {
+				System.out.println(" --- refresh command thread " + Thread.currentThread());
+				return "refresh success !";
+			}
+			
+		};
+		
+		str = refreshCommand.execute();
+		System.out.println(refreshCommand.getClass().getSimpleName() + " return " + str);
+		
 		
 		
 	}
@@ -34,54 +79,4 @@ public class HystrixTest {
 
 }
 
-class HelloCommand extends HystrixCommand<String> {
-	
-	private String name;
-	
-	public HelloCommand(String name) {
-		super(HystrixCommandGroupKey.Factory.asKey(name));
-		
-		this.name = name;
-	}
 
-
-
-
-	@Override
-	protected String run() throws Exception {
-		
-		System.out.println(" --- HelloCommand  调用线程 " + Thread.currentThread());
-		TimeUnit.SECONDS.sleep(30);
-		return "Hello " + name +  " , Nice to meet you !";
-	}
-
-
-
-
-	@Override
-	protected String getFallback() {
-		return "fallback";
-	}
-	
-	
-	
-}
-
-class HelloObserverCommand extends HystrixObservableCommand<String> {
-	
-	private String name ;
-
-	protected HelloObserverCommand(String name) {
-		super(HystrixCommandGroupKey.Factory.asKey(name));
-		this.name = name;
-	}
-
-	@Override
-	protected Observable<String> construct() {
-		
-		System.out.println(" --- HelloObserverCommand  调用线程 " + Thread.currentThread());
-
-		return Observable.just("Hello " + name +  " , Nice to meet you !");
-	}
-	
-}
